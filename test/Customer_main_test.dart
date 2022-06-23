f1bc0c54faa39eed04ae594f7fc6170d62fcfafb
junwei <junweimoo@gmail.com>
2022-06-23 21:01:09 +0800

    Add unit tests for login and signup pages

---
+++ test/Customer_main_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:first_app/Customer_screens/Customer_main.dart';
+
+void main() {
+  testWidgets('Switch between menu, orders, and account on customer', (tester) async {
+
+  });
+}
---
+++ test/Vendor_main_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:first_app/Vendor_screens/Vendor_main.dart';
+
+void main() {
+  testWidgets('Switch between menu, orders, and account on vendor', (tester) async {
+
+  });
+
+  testWidgets('Navigating through canteens, stalls, and menu', (tester) async {
+
+  });
+}
